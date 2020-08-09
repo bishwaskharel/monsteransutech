d8ce775f3a6486f010d1462eaf5f53b0279e0743
Bishwas Kharel <bishukharel98@gmail.com>
2020-08-10 00:32:46 +0545

    Updates

---
+++ precache-manifest.5f65afed32e54778ee87818d3dfa5b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19bade44af17418585b7ab23263403",
+    "url": "/monsteransutech.git/index.html"
+  },
+  {
+    "revision": "3255ec1f3b96cff84e7e",
+    "url": "/monsteransutech.git/static/css/main.4cc6f207.chunk.css"
+  },
+  {
+    "revision": "71051572b7a42ea0fbf2",
+    "url": "/monsteransutech.git/static/js/2.2524815f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsteransutech.git/static/js/2.2524815f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3255ec1f3b96cff84e7e",
+    "url": "/monsteransutech.git/static/js/main.924930c2.chunk.js"
+  },
+  {
+    "revision": "a59ebe55049a38c39bc5",
+    "url": "/monsteransutech.git/static/js/runtime-main.a4b6da87.js"
+  }
+]);
